97b66a8c796b1757b29b8e27d5d0442c3b6fdec1
tahaugurtoyran <tahaugurtoyran5@gmail.com>
2026-03-13 23:09:57 +0300

    creating jwt token. Deleted web and api from docker composer yml

--- src/api/src/app/app.module.ts
+++ src/api/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// Importing our newly created AuthModule
+import { AuthModule } from './auth/auth.module'; 
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule // Registering AuthModule here
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
